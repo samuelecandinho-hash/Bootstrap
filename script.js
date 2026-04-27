d3b8cd32ea4bb4bc50d586fe5321770f092708d8
Roberto <roberto_hille@estudante.sesisenai.org.br>
2026-04-27 11:39:48 -0300

    add

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ let mode = 1
 // 2 = cadastro
 
 document.getElementById("toggle").onclick = () => {
-    if (mode = 1){
+    if (mode == 2){
+        mode = 1;
+    } else if (mode == 1){
         mode = 2;
     }
-    if (mode = 2){
-        mode = 1;
-    }
-    if (mode = 2){
+    if (mode == 2){
     document.getElementById("Titulo").innerText = "Cadastro com Bootstrap";
     }
-    if (mode = 1){
+    if (mode == 1){
     document.getElementById("Titulo").innerText = "Login com Bootstrap";
     }
+    console.log(mode);
 }
 
 document.getElementById("Login001").addEventListener("submit", function(e){
